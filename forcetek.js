8173243b93b4ff93842bb3b07416210a5ea93152
Ell <31101503+elliotttn@users.noreply.github.com>
2019-08-22 15:45:40 +0100

    Exposed new functions

--- forcetek.js
+++ forcetek.js
@@ -1,9 +1,9 @@
 /*
  *  @file Minimal Javascript wrapper for the Force.com REST API
- *  @author Appitek <support@appitek.com>
- *  @copyright Appitek Ltd. 2018
+ *  @author Appitek Ltd. <support@appitek.com>
+ *  @copyright Appitek Ltd. 2017
  *  @license MIT
- *  @version 1.0.0
+ *  @version 1.0.1
  *
  *  @constructor Forcetek(config)
  *  @desc creates a new instance of Forcetek
@@ -17,19 +17,23 @@
  */
 var Forcetek = (function(config) {
   'use strict';
-  var _apiUsage, _apiVersion, _callbackUrl, _client = {}, _consumerKey, 
-    _loginUrl, _popupConfig, _proxyUrl, _watcher;
-  // set default values if no config is passed
+  var _client, 
+      _apiUsage, 
+      _apiVersion, 
+      _callbackUrl, 
+      _consumerKey, 
+      _loginUrl, 
+      _proxyUrl;
+  _client = {};
   if (config) {
     _apiVersion = config.apiVersion || 'v35.0';
     _callbackUrl = config.callbackUrl || null;
     _consumerKey = config.consumerKey || null;
     _loginUrl = config.loginUrl || 'https://login.salesforce.com/';
-    _popupConfig = config.popupConfig || 'height=600, width=400';
     _proxyUrl = config.proxyUrl || null;
-  }
+  };
   /*
-   *  @method _sendRequest() [private]
+   *  @function _sendRequest() [private]
    *  @desc sends a HTTP request to a specified Salesforce REST API endpoint
    *
    *  @param {string} url - REST API path relative to /services/data/
@@ -37,17 +41,26 @@ var Forcetek = (function(config) {
    *  @param {object|null} payload - data to be turned into JSON and sent
    *  @callback - function to call back to in the form of function(err, res)
    *  @param {boolean} retried - used by the function itself, do not add
+   * type for blobs
    */
-  function _sendRequest(url, method, payload, callback, retried) {
-    var err, header, request, xhttp;
+  function _sendRequest(url, method, payload, callback, retried, type) {
+    'use strict';
+    var err, 
+        header, 
+        request, 
+        xhttp;
     if (!_client.session_id) {
       err = {
         errorCode: 'NO_SESSION_ID',
         message: 'There is no session id on this client, please login first.'
       };
       return callback(err, null);
-    }
+    };
     request = _client.instance_url + '/services/data/' + url;
+    if (method == 'GETINFO') {
+      request = url;
+      method = 'GET';
+    }
     xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (xhttp.readyState == 4) {
@@ -56,6 +69,9 @@ var Forcetek = (function(config) {
           // requests like /services/data/ for versions info don't return this
           _apiUsage = xhttp.getResponseHeader('Sforce-Limit-Info') ? 
             xhttp.getResponseHeader('Sforce-Limit-Info').split('=')[1] : _apiUsage;
+          if (type) {
+            return callback(null, xhttp.response || null);
+          }
           callback(null, JSON.parse(xhttp.responseText || null));
         } else {
           /**
@@ -71,6 +87,7 @@ var Forcetek = (function(config) {
            *    REQUIRED_FIELD_MISSING - fields missing from record to C/U
            *    NO_REFRESH_TOKEN - custom, no refresh token is available
            */
+          console.error(xhttp.responseText);
           err = JSON.parse(xhttp.responseText)[0];
           // if we get an invalid session error and we haven't already retried
           // we call a refresh to update the session id and try again
@@ -78,7 +95,7 @@ var Forcetek = (function(config) {
             _refreshClient(function(err, res) {
               if (err) return callback(err, null);
               _sendRequest(url, method, payload, callback, true);
-            });
+            })
           } else {
             callback(err, null);
           }
@@ -91,16 +108,23 @@ var Forcetek = (function(config) {
     xhttp.setRequestHeader('CONTENT-TYPE', 'application/json');
     xhttp.setRequestHeader('SALESFORCEPROXY-ENDPOINT', request);
     xhttp.setRequestHeader(header, 'Bearer ' + _client.session_id);
+    if (type) {
+      console.log(type);
+      xhttp.responseType = type;
+    }
     xhttp.send(JSON.stringify(payload));
-  }
+  };
   /*
-   *  @method _refreshClient() [private]
+   *  @function _refreshClient() [private]
    *  @desc refreshes the _client's access token
    *
    *  @callback - function to call back to in the form of function(err, res)
    */
   function _refreshClient(callback) {
-    var err, request, xhttp;
+    'use strict';
+    var err,
+        request, 
+        xhttp;
     if (!_client.refresh_token) {
       err = {
         errorCode: 'NO_REFRESH_TOKEN',
@@ -108,8 +132,8 @@ var Forcetek = (function(config) {
       };
       return callback(err, null);
     }
-    request = _loginUrl + '/services/oauth2/token?grant_type=refresh_token&client_id=' + 
-      _consumerKey + '&refresh_token=' + _client.refresh_token;
+    request = _loginUrl + '/services/oauth2/token?grant_type=refresh_token&client_id=' 
+      + _consumerKey + '&refresh_token=' + _client.refresh_token;
     xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (xhttp.readyState == 4) {
@@ -119,16 +143,17 @@ var Forcetek = (function(config) {
           _client.session_id = decodeURIComponent(refreshed_token);
           callback(null, _client);
         } else {
-          callback(JSON.parse(xhttp.responseText)[0], null);
+          console.log(xhttp.response);
+          callback(JSON.parse(xhttp.responseText)[0], null)
         }
       }
     };
     xhttp.open('POST', _client.proxy_url || request, true);
     xhttp.setRequestHeader('SALESFORCEPROXY-ENDPOINT', request);
     xhttp.send();
-  }
+  };
   /*
-   *  @method _watchLogin() [private]
+   *  @function _watchLogin() [private]
    *  @desc watches the login window until it reaches the redirect page, then
    *    extracts the url parameters to create the _client 
    *
@@ -138,47 +163,45 @@ var Forcetek = (function(config) {
    *  @callback - function to call back to in the form of function(client)
    */
   function _watchLogin(win, location, callback) {
-    _watcher = setInterval(function() {
+    'use strict';
+    var i,
+        parameters,
+        url;
+    setTimeout(function() {
       // try block as CORS will prevent us reading location until we log in
       // and get redirected to our same-domain callbackUrl
-      if (win.closed == true) { clearInterval(_watcher); }
       try {
         // if we have a location from the cordova InAppBrowser loadstop event
         // use that, otherwise try the current win.location
         url = location ? location : JSON.stringify(win.location);
         if (_callbackUrl && url.includes(_callbackUrl) == true) {
           win.close();
-          clearInterval(_watch);
           parameters = url.split('#')[1].split('&');
           for (i = 0; i < parameters.length; i++) {
-            _client[parameters[i].split('=')[0]] = parameters[i].split('=')[1];
-          }
+            _client[parameters[i].split('=')[0]] = parameters[i].split('=')[1]
+          };
           // need to decode these two or you'll get errors when using them
           _client.instance_url = decodeURIComponent(_client.instance_url);
-          _client.session_id = decodeURIComponent(_client.access_token);
+          _client['session_id'] = decodeURIComponent(_client.access_token);
           // store proxyUrl incase we store the client and use Forcetek.refresh()
-          _client.proxy_url = _proxyUrl;
-          if (callback) { callback(_client); }
+          _client['proxy_url'] = _proxyUrl;
+          // get user info needs fix
+          var userinfo = decodeURIComponent(_client.id);
+          _sendRequest(userinfo, 'GETINFO', null, function(err, res) {
+            _client.email = res.email;
+            if (callback) { callback(_client); }
+          });
+        } else {
+          _watchLogin(win, location, callback);
         }
-      } catch(e) {} 
-    }, 100);
-  }
+      } catch(e) {
+        _watchLogin(win, location, callback);
+      }
+    }, 100)
+  };
   return {
     /*
-     * @method Forcetek.client()
-     * @desc Returns the internal client object so you can access some of it's
-     *   variables, such as the instance_url or access_token
-     * 
-     * @return {object} - object containing the following keys:
-     *   
-     * 
-     * 
-     */
-    client: function() {
-      return _client;
-    },
-    /*
-     *  @method Forcetek.create()
+     *  @function Forcetek.create()
      *  @desc creates a record for a given sObject in Salesforce
      *
      *  @param {string} sobject - type of object to create
@@ -186,11 +209,12 @@ var Forcetek = (function(config) {
      *  @callback - function to call back to in the form of function(err, res)
      */
     create: function(sobject, record, callback) {
+      'use strict';
       var url = _apiVersion + '/sobjects/' + sobject;
       _sendRequest(url, 'POST', record, callback, false);
     },
     /*
-     *  @method Forcetek.del()
+     *  @function Forcetek.del()
      *  @desc deletes a specific record by it's id
      *
      *  @param {string} sobject - type of object that will be deleted
@@ -198,22 +222,28 @@ var Forcetek = (function(config) {
      *  @callback - function to call back to in the form of function(err, res)
      */
     del: function(sobject, id, callback) {
+      'use strict';
       var url = _apiVersion + '/sobjects/' + sobject + '/' + id;
       _sendRequest(url, 'DELETE', null, callback, false);
     },
     /*
-     *  @method Forcetek.login()
+     *  @function Forcetek.login()
      *  @desc opens a window for the user to login, once successfully logged in, 
      *    updates the _client and passes it to the callback for storing
      *
      *  @callback - function to call back to in the form of function(client)
      */
     login: function(callback) {
-      var config, url, win;
+      'use strict';
+      var config, 
+          url, 
+          win;
       url = _loginUrl + 'services/oauth2/authorize?display=popup' +
         '&response_type=token&client_id=' + escape(_consumerKey) + 
         '&redirect_uri=' + escape(_callbackUrl);
-      win = window.open(url, 'Log In', _popupConfig);
+      // @todo maybe expose this config option sometime?
+      config = 'height=600, width=500';
+      win = window.open(url, 'Log In', config);
       // if running cordova we can't access window.location and instead need to
       // listen for the loadstop event that Cordova InAppBrowser adds
       if (window.cordova) {
@@ -225,30 +255,72 @@ var Forcetek = (function(config) {
       }
     },
     /*
-     *  @method Forcetek.query()
+     *  @function Forcetek.query()
      *  @desc sends a SOQL query to the Salesforce REST API
      *
      *  @param {string} soql - the soql query to send
      *  @callback - function to call back to in the form of function(err, res)
      */
     query: function(soql, callback) {
+      'use strict';
       var url = _apiVersion + '/query?q=' + encodeURIComponent(soql);
       _sendRequest(url, 'GET', null, callback, false);
     },
     /*
-     *  @method Forcetek.raw()
+     *  @function Forcetek.query()
+     *  @desc sends a SOQL query to the Salesforce REST API
+     *
+     *  @param {string} soql - the soql query to send
+     *  @callback - function to call back to in the form of function(err, res)
+     */
+    queryAll: function(soql, nextURL, records, callback) {
+      'use strict';
+      if (nextURL != null) {
+        $Force.raw(nextURL, 'GET', null, null, function(err, res) {
+          if (err) return console.error(err);
+          records = records.concat(res.records);
+          if (res.done == false) {
+            nextURL = res.nextRecordsUrl.split('/data/')[1];
+            $Force.queryAll(soql, nextURL, records, callback);
+          } else {
+            callback(records);
+          }
+        });
+      } else {
+        $Force.query(soql, function(err, res) {
+          if (err) return console.error(err);
+          records = records.concat(res.records);
+          if (res.done == false) {
+            nextURL = res.nextRecordsUrl.split('/data/')[1];
+            $Force.queryAll(soql, nextURL, records, callback);
+          } else {
+            callback(records);
+          }
+        });
+      }
+    },
+    // legacy
+    queryBin: function(soql, callback) {
+      'use strict';
+      var url = _apiVersion + '/queryAll/?q=' + encodeURIComponent(soql);
+      _sendRequest(url, 'GET', null, callback, false);
+    },
+    /*
+     *  @function Forcetek.raw()
      *  @desc raw exposure of the _sendRequest to allow custom REST API calls
      *
      *  @param {string} url - REST API path relative to /services/data/
      *  @param {string} method - GET || POST || PUT || PATCH || DELETE || HEAD
+     *  type - for blobs
      *  @param {object | null} payload - data to send for a POST || PATCH
      *  @callback - function to call back to in the form of function(err, res)
      */
-    raw: function(url, method, payload, callback) {
-      _sendRequest(url, method, payload, callback, false);
+    raw: function(url, method, payload, type, callback) {
+      'use strict';
+      _sendRequest(url, method, payload, callback, false, type);
     },
     /*
-     *  @method Forcetek.refresh(client, callback)
+     *  @function Forcetek.refresh(client, callback)
      *  @desc sets the given client and refreshes it, re-activated it
      *  @note you need to have a refresh_token key on the client, which you will
      *    only get from sf oauth if your redirect url is not HTTP/HTTPS or if
@@ -260,23 +332,25 @@ var Forcetek = (function(config) {
      *  @callback - function to call back to in the form of function(err, res)
      */
     refresh: function(client, callback) {
+      'use strict';
       _client = client;
       _refreshClient(callback);
     },
     /*
-     *  @method Forcetek.session()
+     *  @function Forcetek.session()
      *  @desc creates a client from just a session id (for VF page usage)
      *
      *  @param {string} id - session id from VF i.e. using {!$Api.Session_ID}
      */
     session: function(id) {
+      'use strict';
       _client.session_id = id;
       _apiVersion = _apiVersion ? _apiVersion : 'v35.0';
-      // @note does this work for a custom non-sf domain acting as a 301?
+      // @todo does this work for a custom non-sf domain acting as a 301?
       _client.instance_url = window.location.href.split('.com')[0] + '.com';
     },
     /*
-     *  @method Forcetek.update()
+     *  @function Forcetek.update()
      *  @desc updates a specific record with the fields given
      *
      *  @param {string} sobject - type of object to create
@@ -285,21 +359,31 @@ var Forcetek = (function(config) {
      *  @callback - function to call back to in the form of function(err, res)
      */
     update: function(sobject, id, updates, callback) {
+      'use strict';
       var url = _apiVersion + '/sobjects/' + sobject + '/' + id;
       _sendRequest(url, 'PATCH', updates, callback, false);
     },
     /*
-     *  @method Forcetek.usage()
+     *  @function Forcetek.usage()
      *  @desc returns the current REST API usage and limit
      *
      *  @return {object} - contains a 'used' and a 'limit' key, both will be
      *    null if no REST calls have been made in the module's lifetime
-     */
+    */
     usage: function() {
+      'use strict';
       return {
         used: _apiUsage ? _apiUsage.split('/')[0] : null,
         limit: _apiUsage ? _apiUsage.split('/')[1] : null
       };
+    },
+    // return client instance url
+    url: function() {
+      return _client.instance_url;
+    },
+    // return raw client
+    client: function() {
+      return _client;
     }
   };
 });
